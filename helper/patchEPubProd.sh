9b35964d0e2b8d4e13bf9c41ba728c1d1386baf5
7FM <41307817+7FM@users.noreply.github.com>
2023-05-28 21:20:58 +0200

    simplify patchEPubProd

--- helper/patchEPubProd.sh
+++ helper/patchEPubProd.sh
@@ -5,7 +5,6 @@ echo "Patching the EPubProd APK"
 # NOTE: the name may not be altered
 patchedAPK="${SCRIPTPATH}/EPubProd.apk"
 patchFolder="${SCRIPTPATH}/apkPatches"
-apkUnzipDir="${SCRIPTPATH}/EPubProd"
 apkDecomDir="${SCRIPTPATH}/EPubProdDecomp"
 
 # Obtain the EPubProd.apk
@@ -15,31 +14,8 @@ hash=`sha256sum EPubProd.apk`
 cp EPubProd.apk "EPubProd_${hash}.apk"
 echo "Backing up unpatched EPubProd.apk as EPubProd_${hash}.apk"
 
-# unzip apk
-rm -rf "$apkUnzipDir"
-mkdir -p "$apkUnzipDir"
-cd "$apkUnzipDir"
-unzip "$patchedAPK"
-rm "$patchedAPK"
-# Disable error reporting
-sed -i 's/REPORT_ERRORS=.*$/REPORT_ERRORS=FALSE/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-# Disabling some URLs
-sed -i 's/FTU_COUNTRIES_RESELLERS_URL=.*$/FTU_COUNTRIES_RESELLERS_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/FTU_REPORTING_SELECTED_RESELLER_URL=.*$/FTU_REPORTING_SELECTED_RESELLER_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-# sed -i 's/UPDATE_CHECK_URL=.*$/UPDATE_CHECK_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/HOTSPOT_NETDATA_URL=.*$/HOTSPOT_NETDATA_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/PING_URL=.*$/PING_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/INVENTORY_URL=.*$/INVENTORY_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/FAMILY_URL=.*$/FAMILY_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/FAMILY_RESELLERS_URL=.*$/FAMILY_RESELLERS_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/FEEDBACK_URL=.*$/FEEDBACK_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/LOST_DATA_RECOVERY_URL=.*$/LOST_DATA_RECOVERY_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-sed -i 's/RECOMMENDATIONS_URL=.*$/RECOMMENDATIONS_URL=0.0.0.0/' "${apkUnzipDir}/assets/environments/app.properties.prod"
-
-# Zip apk again!
-zip -r "$patchedAPK" *
-
 # Decompile APK
+rm -rf "$apkDecomDir"
 mkdir -p "$apkDecomDir"
 cd "$apkDecomDir"
 apktool d "$patchedAPK"
@@ -49,6 +25,21 @@ sed -i 's/To my books/Library/g' "res/values/strings.xml"
 sed -i 's/My books/Library/g' "res/values/strings.xml"
 sed -i 's/To my books/Library/g' "res/values-en/strings.xml"
 sed -i 's/My books/Library/g' "res/values-en/strings.xml"
+
+# Disable error reporting
+sed -i 's/REPORT_ERRORS=.*$/REPORT_ERRORS=FALSE/' "assets/environments/app.properties.prod"
+# Disabling some URLs
+sed -i 's/FTU_COUNTRIES_RESELLERS_URL=.*$/FTU_COUNTRIES_RESELLERS_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/FTU_REPORTING_SELECTED_RESELLER_URL=.*$/FTU_REPORTING_SELECTED_RESELLER_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+# sed -i 's/UPDATE_CHECK_URL=.*$/UPDATE_CHECK_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/HOTSPOT_NETDATA_URL=.*$/HOTSPOT_NETDATA_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/PING_URL=.*$/PING_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/INVENTORY_URL=.*$/INVENTORY_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/FAMILY_URL=.*$/FAMILY_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/FAMILY_RESELLERS_URL=.*$/FAMILY_RESELLERS_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/FEEDBACK_URL=.*$/FEEDBACK_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/LOST_DATA_RECOVERY_URL=.*$/LOST_DATA_RECOVERY_URL=0.0.0.0/' "assets/environments/app.properties.prod"
+sed -i 's/RECOMMENDATIONS_URL=.*$/RECOMMENDATIONS_URL=0.0.0.0/' "assets/environments/app.properties.prod"
 
 # Apply patches/changes
 for i in "${patchFolder}"/*.patch; do
