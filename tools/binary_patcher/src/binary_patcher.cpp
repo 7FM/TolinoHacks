fb0d2e61f40b4bf19ffdb0114274539953b98a04
7FM <41307817+7FM@users.noreply.github.com>
2023-05-29 18:09:17 +0200

    also patch call to setgroups

--- tools/binary_patcher/src/binary_patcher.cpp
+++ tools/binary_patcher/src/binary_patcher.cpp
@@ -6,7 +6,7 @@
 #define NUM_ELEM(x) sizeof(x) / sizeof(x[0])
 
 int main(int argc, char **argv) {
-  constexpr unsigned expectedOccurrences[] = {2, 1};
+  constexpr unsigned expectedOccurrences[] = {1, 2, 1};
 
   // Thanks to https://cweiske.de/tagebuch/android-root-adb.htm for the write up
   // This moves the value 2000 (the unprivileged UID/GID) to the register r0
@@ -14,6 +14,8 @@ int main(int argc, char **argv) {
   // Hence, we want to replace this instruction as well as the following
   // syscalls with NOPs to gain root privileges
   constexpr uint8_t searchSequence[][4] = {
+      // ARMv8 thumb: blx 0x17ddc aka blx setgroups
+      {0x0E, 0xF0, 0xAC, 0xED},
       // ARMv8 thumb: mov.w r0, #0x7d0
       {0x4f, 0xf4, 0xfa, 0x60},
       // {0xf4, 0x4f, 0x60, 0xfa},
@@ -22,6 +24,13 @@ int main(int argc, char **argv) {
       // {0xf0, 0x0e, 0xed, 0xde},
   };
   constexpr uint8_t replaceSeq1[] = {
+      // ARMv8 thumb: nop, nop for: blx 0x17ddc / blx syscall
+      0x00, 0xbf, 0x00, 0xbf,
+      // nop for return value check: cmp r0,#0x0
+      0x00, 0xbf,
+      // nop for failure jump: bne failure
+      0x00, 0xbf};
+  constexpr uint8_t replaceSeq2[] = {
       // ARMv8 thumb: nop, nop for: mov.w r0, #0x7d0
       0x00, 0xbf, 0x00, 0xbf,
       // nop, nop for: blx syscall
@@ -30,16 +39,17 @@ int main(int argc, char **argv) {
       0x00, 0xbf,
       // nop for failure jump: bne failure
       0x00, 0xbf};
-  constexpr uint8_t replaceSeq2[] = {
-      // ARMv8 thumb: nop, nop for: mov.w r0, #0x7d0
+  constexpr uint8_t replaceSeq3[] = {
+      // ARMv8 thumb: nop, nop for: blx	0x17dfc aka blx prctl
       0x00,
       0xbf,
       0x00,
       0xbf,
   };
-  const uint8_t *const replaceSequence[] = {replaceSeq1, replaceSeq2};
-  constexpr unsigned replaceSequenceSizes[] = {NUM_ELEM(replaceSeq1),
-                                               NUM_ELEM(replaceSeq2)};
+  const uint8_t *const replaceSequence[] = {replaceSeq1, replaceSeq2,
+                                            replaceSeq3};
+  constexpr unsigned replaceSequenceSizes[] = {
+      NUM_ELEM(replaceSeq1), NUM_ELEM(replaceSeq2), NUM_ELEM(replaceSeq3)};
 
   if (argc != 3) {
     std::cerr << "Invalid argument count!" << std::endl;
